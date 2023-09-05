706e16f3e6751a054bc8ee19a32496c1efd0fdc4
Tyler Bauman <t.bauman@vehikl.com>
2023-09-05 09:34:38 -0400

    Initial commit

---
+++ apps/client/src/app/app.tsx
@@ -0,0 +1,13 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <style jsx>{`
+        /** your style here **/
+      `}</style>
+    </div>
+  );
+}
+
+export default App;
